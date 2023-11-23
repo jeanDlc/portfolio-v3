af1fab1197a802f4b867fb5fc0cbc113a2432674
Jean Pierre <jeanpierre101298@gmail.com>
2023-11-23 12:32:18 -0500

    Feature / make Project card clickacble

--- src/components/ProjectCard/index.tsx
+++ src/components/ProjectCard/index.tsx
@@ -1,26 +1,41 @@
-import { Project } from "@/types";
 import Image from "next/image";
 import ClipIcon from "@/components/icons/clip";
 import githubSvg from "@/assets/github.svg";
-import srcMock from "@/assets/mock-image.svg";
 import styles from "./styles.module.scss";
 import { baiJamjuree } from "@/fonts";
+import UpRightArrowIcon from "@/components/icons/up-right-arrow";
+
+import type { Project } from "@/types";
 
 export default function ProjectCard({ project }: { project: Project }) {
   const { description, name, stack, demo, repository, image } = project;
 
   return (
     <article className={styles.container}>
-      <header className={styles.cover}>
-        <Image src={image.url} fill alt={`Preview of project ${name}`} />
-      </header>
-      <h3 className={`${baiJamjuree.className} ${styles.title}`}> {name} </h3>
-      <p className={styles.description}> {description} </p>
-      <ul className={styles.stack}>
-        {stack.map((tech) => (
-          <li key={tech}> {tech} </li>
-        ))}
-      </ul>
+      <a
+        className={styles.action}
+        href={demo}
+        target="_blank"
+        rel="noreferrer nofollow"
+      >
+        <header className={styles.cover}>
+          <Image src={image.url} fill alt={`Preview of project ${name}`} />
+        </header>
+        <h3 className={`${baiJamjuree.className} ${styles.title}`}>
+          {" "}
+          {name}{" "}
+          <span className={styles.redirect}>
+            <UpRightArrowIcon />
+          </span>{" "}
+        </h3>
+
+        <p className={styles.description}> {description} </p>
+        <ul className={styles.stack}>
+          {stack.map((tech) => (
+            <li key={tech}> {tech} </li>
+          ))}
+        </ul>
+      </a>
       <footer className={styles.links}>
         <a
           href={demo}
